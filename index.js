4765856936d7e9eaba0adcf7079f2e28ed7cb618
Daniel Boros <daniel.boros@online.de>
2019-11-09 19:24:20 +0100

    deploying attempt

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 app.use(express.static("public"));
 app.use(express.static("css"));
 app.use(express.static("img"));
 
-
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(3000);
