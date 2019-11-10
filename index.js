478bcc4461eb0423b9bb601ae3d5d6d9307c1584
Daniel Boros <daniel.boros@online.de>
2019-11-10 12:50:58 +0100

    commit before deploying

--- index.js
+++ index.js
@@ -12,16 +12,33 @@ app.get('/cities', function(req, res) {
     res.render("cities-main");
 });
 
+app.get('/cities/population', function(req, res) {
+    res.render("table");
+});
+
 app.get('/cities/migration', function(req, res) {
     res.render("cities");
+});
+
+app.get('/cities/area', function(req, res) {
+    res.render("cities-area");
 });
 
 app.get('/countries', function(req, res) {
     res.render("countries-main");
 });
 
+app.get('/countries/trade', function(req, res) {
+    res.render("trade");
+});
 
+app.get('/countries/population', function(req, res) {
+    res.render("countries-population");
+});
 
+app.get('/countries/area', function(req, res) {
+    res.render("countries-area");
+});
 
-
-app.listen(process.env.PORT, process.env.IP);
+app.listen(3000);
+// app.listen(process.env.PORT, process.env.IP);
